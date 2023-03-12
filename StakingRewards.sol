4ef703d9d2140e9160183972ef51deb9d6d53af8
14si20 <14si2o@proton.me>
2023-03-12 09:02:41 +0100

    added functionality notifyRewardAmount

--- StakingRewards.sol
+++ StakingRewards.sol
@@ -20,14 +20,42 @@ contract StakingRewards {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not the owner");
+        _;
+    }
+
     constructor(address _stakingToken, address _rewardsToken){
         owner = msg.sender;
         stakingToken = IERC20(_stakingToken);
         rewardsToken = IERC20(_rewardsToken);
     }
 
-    function setRewardsDuration(uint _duration) external {}
-    function notifyRewardAmount(uint _amount) external {}
+    function setRewardsDuration(uint _duration)  external onlyOwner {
+        require(finishAt < block.timestamp, "the rewards duration is not finished");
+        duration = _duration;
+
+    }
+    function notifyRewardAmount(uint _amount) external onlyOwner {
+        if(block.timestamp > finishAt){
+            rewardRate = _amount / duration;
+        } else {
+            uint remainingRewards = rewardRate * (finishAt - block.timestamp);
+            rewardRate = (remainingRewards + _amount) / duration;
+        }
+
+        require(rewardRate > 0, "reward rate equals to zero" );
+        require(
+            rewardRate * duration <= rewardsToken.balanceOf(address(this)),
+            "reward amount > balance"
+        );
+
+        finishAt = block.timestamp + duration;
+        updatedAt = block.timestamp;
+         
+
+
+    }
     function stake(uint _amount) external {}
     function withdraw(uint _amount) external {}
     function earned(address _account) external view returns (uint) {}
